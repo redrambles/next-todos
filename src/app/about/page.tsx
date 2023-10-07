3eada9cc1c9439e4354f7b4a5560f1b5ca8d2315
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2023-10-07 14:52:12 -0400

    Update about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from "react";
 import Link from "next/link";
 
@@ -10,27 +11,23 @@ function AboutMe() {
 
         <p className='my-4'>
           I'm Ann, a musician turned full-stack developer with over 8 years of building web applications and remote teaching
-          experience, particularly with folks who are transitioning into the tech field as early-career developers. I teach HTML /
-          CSS, Git, JavaScript, and React. Troubleshooting, tinkering, breaking down problems into manageable pieces, and
-          conveying the information in a 'meet-you-where-you-are' kind of way is what I spend a lot of my time doing. 
+          experience, particularly with folks who are transitioning into the tech field as early-career developers.
         </p>
 
         <p className='my-4'>
           I also have extensive experience building and maintaining web applications using a variety of stacks. I've recently
           fallen in love with Ruby on Rails, working with a friend on an open-source environment research initiative over the past
-          year. It has given me the opportunity to learn so much about the language and framework, as well as the general flow of
-          working on a team, from issue to pull request.
+          year.
         </p>
 
         <p className='my-4'>
           I'm looking for an opportunity to push my technical skills to the next level! I am perpetually curious, always seeking
-          to learn more and thrive in a team setting. I believe I bring a fresh perspective and an open mind to problem-solving
-          and working with software.{" "}
+          to learn more and thrive in a team setting. Right now I'm wrapping my mind around Next.js 13. 🎉
         </p>
       </article>
       <Link
         href='/'
-        className='inline-block py-2 my-4 border border-slate-300 text-slate-300 px-2 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none'
+        className='inline-block py-2 my-4 border border-orange-300 text-orange-300 px-2 rounded hover:bg-orange-700 hover:text-orange-200 focus-within:bg-orange-700 outline-none'
       >
         Back to Home Page
       </Link>
